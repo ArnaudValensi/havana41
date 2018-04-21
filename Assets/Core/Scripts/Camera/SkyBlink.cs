f0c5ec8f2582dbb772405156b88944f9ca35c722
Arnaud Valensi <arnaud.valensi@gmail.com>
2018-04-21 13:01:59 +0200

    add the player

---
+++ Assets/Core/Scripts/Camera/SkyBlink.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class SkyBlink : MonoBehaviour {
+
+	public Color blinkColor = Color.white;
+	public float duration = 0.2f;
+
+	Camera cameraToBlink;
+	Color originalColor;
+
+	void Start () {
+		cameraToBlink = GetComponent<Camera>();
+
+		originalColor = cameraToBlink.backgroundColor;
+	}
+	
+	public void Blink () {
+		StartCoroutine(BlinkCoroutine());
+	}
+
+	IEnumerator BlinkCoroutine() {
+		cameraToBlink.backgroundColor = blinkColor;
+		yield return new WaitForSeconds(duration);
+		cameraToBlink.backgroundColor = originalColor;
+	}
+}
