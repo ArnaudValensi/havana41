e326627c0085f5cab0856bac1a733dde74981fa0
Red <roussel.kevin@outlook.com>
2018-04-22 13:03:42 +0200

    Implement Coin Collect event

--- Assets/Coin.cs
+++ Assets/Coin.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Coin : MonoBehaviour {
 
     [SerializeField] int ScoreUpgrade = 10000;
+    [SerializeField] UnityEvent onCollect;
 
     void OnTriggerEnter2D(Collider2D col)
     {
         if (col.transform.tag == "Player")
         {
+            onCollect.Invoke();
             ScoreBanner.Instance.AddScore(ScoreUpgrade);
             Destroy(gameObject);
         }
