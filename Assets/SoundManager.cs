dbfd5a7c0ac775befa0e93d336bc6462fd50be64
Red <roussel.kevin@outlook.com>
2018-04-22 15:15:07 +0200

    coin Sound Integration

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    #region InternalStruct
+    [System.Serializable]
+    class NameAndSound
+    {
+        public string name;
+        public AudioSource audio;
+    }
+    #endregion
+    public static SoundManager Instance;
+    [SerializeField] List<NameAndSound> conf;
+
+    private void Start()
+    {
+        Instance = this;
+    }
+
+    public void PlaySound(string name)
+    {
+        conf.Where(i => i.name == name).FirstOrDefault()?.audio.Play();
+    }
+
+
+}
