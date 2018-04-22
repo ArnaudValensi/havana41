b44a9a61def8c4b03895cc17bbd7bcfffb654ca5
Red <roussel.kevin@outlook.com>
2018-04-22 16:30:08 +0200

    Speed support

---
+++ Assets/SpeedDisplay.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SpeedDisplay : MonoBehaviour {
+
+    [SerializeField] TextMeshProUGUI _text;
+
+    [SerializeField]  float Min = 0.8f;
+    [SerializeField]  float Max = 0f;
+
+    private void Update()
+    {
+        var t = (int)Mathf.Abs((((HavanaManager.Instance.GlobalTransitionInterval - Max) / (Min - Max))  *100) - 100)+1;
+        _text.text = t.ToString();
+    }
+}
