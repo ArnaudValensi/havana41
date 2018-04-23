b951f765f4a75b4edd88478e3ebb9031db13b7cb
Arnaud Valensi <arnaud.valensi@gmail.com>
2018-04-23 18:21:23 +0200

    improve main animator animations

---
+++ Assets/Core/Scripts/Game/ArenaManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArenaManager : MonoBehaviour {
+
+	public Animator animator;
+
+	void Start() {
+		animator = GetComponent<Animator>();
+	}
+
+	public void GameOverToTitle() {
+		Debug.Log("GameOverToTitle");
+
+		animator.SetInteger("InternalState", 0);
+	}
+
+}
